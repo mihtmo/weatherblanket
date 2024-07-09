016d6c46daab4cdc9a58db7efb5a836b89f1d0a6
wilchett <mitch.webb@att.net>
2024-07-08 20:21:05 -0500

    getting rid of old approach

--- frontend/src/components/WeatherBars.js
+++ frontend/src/components/WeatherBars.js
@@ -18,33 +18,3 @@ export const WeatherBars = ({ blanketData, selectedYears }) => {
         </div>
     )
 }
-
-const DayBar = ( {data, dayNum, maxRain}) => {
-
-    const highColor = data ? heatColorScale(data['MAX']) : 'none';
-    const lowColor = data ? heatColorScale(data['MIN']) : 'none';
-    const rainCoef = (data && maxRain) ? rainScale(data['PRCP'], maxRain) : null;
-
-    return (
-        <div 
-            key={dayNum}
-            id={`day${dayNum}`} 
-            className='day-bar' 
-            data-index={dayNum}
-        >
-            <div 
-                id={`temp-bar${dayNum}`} 
-                className='blanket-column heat-column'
-                style={{
-                    'backgroundImage' : `linear-gradient(${highColor}, ${lowColor})`
-                }}/>
-            <div
-                id={`rain-bar${dayNum}`}
-                className='blanket-column rain-column'
-                style={ data ? {
-                    'backgroundImage' : `linear-gradient(${lowColor} 60%, #2C48B8)`,
-                    'flexBasis': `${rainCoef * 40}vh`,
-                } : {}}/>
-        </div>
-    )
-}
